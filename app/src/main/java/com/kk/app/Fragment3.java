603dba4d5f8f18b5e4cf7ee618418dc18e2e0104
wudifamo <125282834@qq.com>
2016-05-05 17:18:25 +0800

    4

--- app/src/main/java/com/kk/app/Fragment3.java
+++ app/src/main/java/com/kk/app/Fragment3.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.ramotion.foldingcell.FoldingCell;
@@ -15,6 +16,7 @@ import com.ramotion.foldingcell.FoldingCell;
 public class Fragment3 extends Fragment {
     private View viewFragment;
     Context context;
+    private FrameLayout frame3_image;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,19 +29,22 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         viewFragment = inflater.inflate(R.layout.fragment3, null);
-//        TextView tv = (TextView)viewFragment.findViewById(R.id.tv);
-//        tv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(context,CameraActivity.class));
-//            }
-//        });
         final FoldingCell fc = (FoldingCell) viewFragment.findViewById(R.id.folding_cell);
         final FoldingCell fc1 = (FoldingCell) viewFragment.findViewById(R.id.folding_cell1);
+        final FoldingCell fc2 = (FoldingCell) viewFragment.findViewById(R.id.folding_cell2);
+        frame3_image = (FrameLayout) viewFragment.findViewById(R.id.frame3_image);
         fc.initialize(1500, Color.WHITE, 4);
+        fc2.initialize(1500, Color.WHITE, 7);
         // attach click listener to folding cell
         fc.setOnClickListener(new expendListener());
         fc1.setOnClickListener(new expendListener());
+        fc2.setOnClickListener(new expendListener());
+        frame3_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, CameraActivity.class));
+            }
+        });
         return viewFragment;
     }
 
